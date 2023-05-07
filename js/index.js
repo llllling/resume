e0c4911a3ea7468a0604190f3b3814e7dc79c72c
MISEONGKIM <rlaaltjd46@naver.com>
2023-05-07 21:22:08 +0900

    update : 이미지 원래대로 이벤트 추가

--- js/index.js
+++ js/index.js
@@ -1,23 +1,24 @@
 import { Photo } from "./photo.js";
 const photo = new Photo();
-const nudake = document.querySelector(".nudake");
+const nudake = document.querySelector(".canvas_bg");
 const resetBtn = document.querySelector(".reset");
 
 function onMousedown(e) {
   nudake.addEventListener("mousemove", onMouseMove);
   nudake.addEventListener("mouseup", onMouseUp);
-  resetBtn.addEventListener("click", () => {
-    photo.drawImage();
-  });
+  window.addEventListener("mouseup", onMouseUp);
+  //이벤트 함수는 내부적으로 this가 해당 엘리먼트로 바뀜
+  resetBtn.addEventListener("click", photo.drawImage.bind(photo));
+  photo.prePosition.x = e.offsetX;
+  photo.prePosition.y = e.offsetY;
 }
 
 function onMouseUp() {
   nudake.removeEventListener("mousemove", onMouseMove);
   nudake.removeEventListener("mouseup", onMouseUp);
+  window.removeEventListener("mouseup", onMouseUp);
   nudake.removeEventListener("mouseleave", onMouseUp);
-  resetBtn.removeEventListener("click", () => {
-    photo.drawImage();
-  });
+  resetBtn.removeEventListener("click", photo.drawImage.bind(photo));
 }
 function onMouseMove(e) {
   photo.drawCircles(e);
--- js/photo.js
+++ js/photo.js
@@ -10,27 +10,39 @@ export class Photo {
     this.canvas.height = this.h;
     this.canvas.style.width = this.w + "px";
     this.canvas.style.height = this.h + "px";
-    this.drawImage();
-  }
 
-  drawImage() {
-    this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
-    const img = new Image();
-    img.src = "../images/my1.jpg";
-    img.onload = () => {
-      this.ctx.drawImage(img, 0, 0, this.w, this.h);
+    this.img = new Image();
+    this.img.src = "../images/my1.jpg";
+    this.img.onload = () => {
+      this.drawImage();
     };
   }
 
+  drawImage() {
+    // 기본값, 대상값이 위로 ; 즉 처음 그려진 도형 위에 나중에 그려진 도형이 표시 됨
+    this.ctx.globalCompositeOperation = "source-over";
+    this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    this.ctx.drawImage(this.img, 0, 0, this.canvas.width, this.canvas.height);
+  }
+
   drawCircles(e) {
-    console.log("drawCircles", e.offsetX, e.offsetY);
+    //원본 그림이 겹쳐지지 않음 부분만 ; 즉 처음에 그려진 도형에서 겹쳐지지 않는 부분만 표시 됨
     this.ctx.globalCompositeOperation = "destination-out";
 
     const newPosition = { x: e.offsetX, y: e.offsetY };
-    const distance = this.ctx.beginPath();
-    this.ctx.arc(e.offsetX, e.offsetY, 20, 0, Math.PI * 2);
-    this.ctx.fill();
-    this.ctx.closePath();
+    const dx = Math.pow(this.prePosition.x - newPosition.x, 2);
+    const dy = Math.pow(this.prePosition.y - newPosition.y, 2);
+    const distance = Math.sqrt(dx + dy);
+    const angle = Math.atan2(dy, dx);
+    Array.from({ length: distance }, (v, i) => i + 1).forEach((index) => {
+      const x = this.prePosition.x + Math.cos(angle) * index;
+      const y = this.prePosition.y + Math.sin(angle) * index;
+      this.ctx.beginPath();
+      this.ctx.arc(x, y, 20, 0, Math.PI * 2);
+      this.ctx.fill();
+      this.ctx.closePath();
+    });
+    this.prePosition = newPosition;
   }
   checkPercent() {}
 }
