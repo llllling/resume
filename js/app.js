589f4e7a500e4cebcfc00d8404e7bcdb4cef24f4
MISEONGKIM <rlaaltjd46@naver.com>
2023-06-04 19:12:36 +0900

    update : 색상 수정 및 app class 이벤트등록 bind 제거

--- js/app.js
+++ js/app.js
@@ -8,33 +8,33 @@ export class App {
     this.resetBtn = document.querySelector(".reset");
   }
   init() {
-    this.nudake.addEventListener("mousedown", this.onMousedown.bind(this));
+    this.nudake.addEventListener("mousedown", this.onMousedown);
   }
 
-  onMousedown(e) {
-    this.nudake.addEventListener("mousemove", this.onMouseMove.bind(this));
-    this.nudake.addEventListener("mouseup", this.onMouseUp.bind(this));
-    window.addEventListener("mouseup", this.onMouseUp.bind(this));
+  onMousedown = (e) => {
+    this.nudake.addEventListener("mousemove", this.onMouseMove);
+    this.nudake.addEventListener("mouseup", this.onMouseUp);
+    window.addEventListener("mouseup", this.onMouseUp);
     //이벤트 함수는 내부적으로 this가 해당 엘리먼트로 바뀜
     this.resetBtn.addEventListener(
       "click",
       this.photo.drawImage.bind(this.photo)
     );
     this.photo.initPosition(e);
-  }
+  };
 
-  onMouseUp() {
-    this.nudake.removeEventListener("mousemove", this.onMouseMove.bind(this));
-    this.nudake.removeEventListener("mouseup", this.onMouseUp.bind(this));
-    window.removeEventListener("mouseup", this.onMouseUp.bind(this));
-    this.nudake.removeEventListener("mouseleave", this.onMouseUp.bind(this));
+  onMouseUp = () => {
+    this.nudake.removeEventListener("mousemove", this.onMouseMove);
+    this.nudake.removeEventListener("mouseup", this.onMouseUp);
+    window.removeEventListener("mouseup", this.onMouseUp);
+    this.nudake.removeEventListener("mouseleave", this.onMouseUp);
     this.resetBtn.removeEventListener(
       "click",
       this.photo.drawImage.bind(this.photo)
     );
-  }
+  };
 
-  onMouseMove(e) {
+  onMouseMove = (e) => {
     this.photo.drawCircles(e);
-  }
+  };
 }
