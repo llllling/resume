bc6a359f4efd204f4d5a408dc106781be0012a11
MISEONGKIM <rlaaltjd46@jinhakapply.com>
2023-06-29 16:53:19 +0900

    update : create canvas로 변경

--- js/Browser.js
+++ js/Browser.js
@@ -3,7 +3,7 @@ import { Photo } from "./Photo.js";
 export class Browser {
   photoSize = 400;
   constructor() {
-    this.canvas = this.initCanvas();
+    this.canvas = this.createCanvas();
     this.canvas.addEventListener("mousedown", this.onMousedown);
 
     this.photo = new Photo(this.canvas);
@@ -11,12 +11,14 @@ export class Browser {
     this.resetBtn = document.getElementsByClassName("reset")[0];
   }
 
-  initCanvas() {
-    const canvas = document.getElementsByTagName("canvas")[0];
+  createCanvas() {
+    const canvas = document.createElement("canvas");
     canvas.width = this.photoSize;
     canvas.height = this.photoSize;
     canvas.style.width = this.photoSize + "px";
     canvas.style.height = this.photoSize + "px";
+
+    document.getElementsByClassName("canvas_bg")[0].appendChild(canvas);
 
     return canvas;
   }
--- js/Mobile.js
+++ js/Mobile.js
@@ -2,7 +2,6 @@ export class Mobile {
   constructor() {
     document.getElementsByClassName("bgimg")[0].style.display = "none";
     document.getElementsByClassName("reset")[0].style.display = "none";
-    document.getElementsByTagName("canvas")[0].style.display = "none";
     document.getElementsByClassName("canvas_bg")[0].style.backgroundImage =
       'url("../images/my1.jpg")';
   }
